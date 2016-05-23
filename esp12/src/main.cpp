9d1e972b9a23c415c5f6d74df8af639c6f4652d1
Daniel Andrade <daniel@danielandrade.net>
2016-05-24 01:21:38 +0200

    Add fadeToColor function. May be inverted for now.

--- esp12/src/main.cpp
+++ esp12/src/main.cpp
@@ -16,11 +16,47 @@
 ThingerWebConfig thing;
 
 // Pins
-const int leds[4] = {13,12,14,16}; // RED(D7) - GREEN(D6) - BLUE(D5) - WHITE(D0)
-const int reset = 0;
+int leds[4] = {13,12,14,16}; // RED(D7) - GREEN(D6) - BLUE(D5) - WHITE(D0)
+int reset = 0;
 
 // Variables
-int color[4] = {0,0,0,0}; // red green blue white
+int fadeSpeed = 15;                 //fading delay
+// #define maxVal(a,b) ((a)>(b)?(a):(b));  // Replacement for max()
+int oldC[4] = {0,0,0,0};                // Old colors
+int color[4] = {0,0,0,0};               // Red Green Blue White
+
+
+// Set New Color
+void setColor(int* leds,int* color)
+{
+  for(int i = 0; i <= 3; i++)
+  {
+    Serial.print("Changing LED: ");
+    Serial.print(leds[i]);
+    Serial.print(" to ");
+    Serial.println(color[i]);
+    Serial.println("---");
+  }
+}
+
+void fadeToColor(int* led, int* startColor, int* endColor, int fadeSpeed){
+ int changeRed   = endColor[0] - startColor[0];                                  //the difference in the two colors for the red channel
+ int changeGreen = endColor[1] - startColor[1];                                  //the difference in the two colors for the green channel
+ int changeBlue  = endColor[2] - startColor[2];
+ int changeWhite = endColor[3] - startColor[3];                                  //the difference in the two colors for the blue channel
+ int steps = max(abs(changeRed),max(abs(changeGreen), abs(changeBlue)));         //make the number of change steps the maximum channel change
+
+ for(int i = 0 ; i < steps; i++){                                                //iterate for the channel with the maximum change
+   int newRed      = startColor[0] + (i * changeRed / steps);                   //the newRed intensity dependant on the start intensity and the change determined above
+   int newGreen    = startColor[1] + (i * changeGreen / steps);                 //the newGreen intensity
+   int newBlue     = startColor[2] + (i * changeBlue / steps);
+   int newWhite    = startColor[3] + (i * changeWhite / steps);                  //the newBlue intensity
+   int newColor[]  = {newRed, newGreen, newBlue, newWhite};                     //Define an RGB color array for the new color
+   setColor(leds, newColor);
+   delay(fadeSpeed);                                                             //Set the LED to the calculated value
+ }
+}
+
 
 void setup()
 {
@@ -38,22 +74,21 @@ void setup()
 
   // Everytime a websocket msg is received
   thing["colors"] << [](pson& in){
+
+    // Update old color array
+    for(int i=0;i<=3;i++)
+    {
+      oldC[i] = color[i];
+    }
+
     color[0] = in["red"];
     color[1] = in["green"];
     color[2] = in["blue"];
     color[3] = in["white"];
 
-    Serial.print(millis());
-    Serial.print(" - Red: ");
-    Serial.print(color[0]);
-    Serial.print(" - Green: ");
-    Serial.print(color[1]);
-    Serial.print(" - Blue: ");
-    Serial.print(color[2]);
-    Serial.print(" - White: ");
-    Serial.println(color[3]);
-  };
+    fadeToColor(leds, color, oldC, fadeSpeed);
 
+  };
 }
 
 void loop()
