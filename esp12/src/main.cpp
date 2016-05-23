c9d86a899a71758ee2b36f1b27b836f60af1e7ac
Daniel Andrade <daniel@danielandrade.net>
2016-05-24 00:33:32 +0200

    Building system and serial communication working.

--- esp12/src/main.cpp
+++ esp12/src/main.cpp
@@ -9,128 +9,54 @@
 #include <WiFiManager.h>
 #include <ThingerWebConfig.h>
 
-// Auto Config
-ThingerWebConfig thing;
-
 // Debug
 #define _DEBUG_
 
+// Auto Config
+ThingerWebConfig thing;
+
 // Pins
-const int leds[4] = {14,12,13,1}; // RED GREEN BLUE WHITE
-const int RESET = 0;
+const int leds[4] = {13,12,14,16}; // RED(D7) - GREEN(D6) - BLUE(D5) - WHITE(D0)
+const int reset = 0;
 
 // Variables
-const int mydelay = 15;
-#define maxVal(a,b) ((a)>(b)?(a):(b));  // Replacement for max()
-int newC[4] = {0,0,0,0};                // red green blue white
-int oldC[4] = {0,0,0,0};                // old colors
-
-// void setColor(int* led, int* color)
-// {
-//   for(int i=0;i<=3;i++){
-//     Serial.print("LED: ");
-//     Serial.print(i);
-//     Serial.print(" to ");
-//     Serial.println(color[i]);
-//     //    analogWrite(led[i], color[i]);
-//   }
-// }
-
+int color[4] = {0,0,0,0}; // red green blue white
 
 void setup()
 {
-
   // Serial YO!
   Serial.begin(115200);
+  Serial.println("Sale lamp starting...");
 
-  //Led as output
-  for(int i=0;i<=3;i++)
+  //Reset option
+  if(reset==1)
   {
-    pinMode(leds[i],OUTPUT);
-    }
-
-  // pinMode(RESET,INPUT);  // Reset switch
-
-  // Check for reset button
-  if(RESET==1)
-  {
+    Serial.println("RESET ON!");
     WiFiManager wifiManager;
     wifiManager.resetSettings();
-    Serial.println("RESET ON!");
   }
 
-  thing["colors"] << [](pson& in)
-  {
+  // Everytime a websocket msg is received
+  thing["colors"] << [](pson& in){
+    color[0] = in["red"];
+    color[1] = in["green"];
+    color[2] = in["blue"];
+    color[3] = in["white"];
 
-    // Set Old Color
-    for(int i=0;i<=3;i++)
-    {
-      oldC[i] = newC[i];
-    }
-
-    // Get values from WebSocket
-    newC[1] = in["red"];
-    newC[2] = in["green"];
-    newC[0] = in["blue"];
-    newC[3] = in["white"];
-
-
-//    // Print values
-//    Serial.print(millis());
-//    Serial.print(" - Red: ");
-//    Serial.print(newC[0]);
-//    Serial.print(" - Green: ");
-//    Serial.print(newC[1]);
-//    Serial.print(" - Blue: ");
-//    Serial.print(newC[2]);
-//    Serial.print(" - White: ");
-//    Serial.println(newC[3]);
-//
-//    fadeToColor(leds,oldC,newC,mydelay );
-      // setColor(leds,newC);
-
+    Serial.print(millis());
+    Serial.print(" - Red: ");
+    Serial.print(color[0]);
+    Serial.print(" - Green: ");
+    Serial.print(color[1]);
+    Serial.print(" - Blue: ");
+    Serial.print(color[2]);
+    Serial.print(" - White: ");
+    Serial.println(color[3]);
   };
 
 }
 
 void loop()
 {
-  // Thinger
   thing.handle();
 }
-
-
-
-
-//void setColor(int* led, byte* color){
-//  for(int i = 0; i < 3; i++){
-//    // analogWrite(led[i], color[i]);
-//    Serial.print("LED ");
-//    Serial.print(led[i]);
-//    Serial.print(" with color ");
-//    Serial.print(color[i]);
-//  }
-//}
-//
-//void setColor(int* led, const byte* color){
-//  byte tempByte[] = {color[0], color[1], color[2]};
-//  setColor(newC, tempByte);
-//}
-
-
-//void fadeToColor(int* led, byte* startColor, byte* endColor, int fadeSpeed){
-//  int changeRed   = endColor[0] - startColor[0];                                  //the difference in the two colors for the red channel
-//  int changeGreen = endColor[1] - startColor[1];                                  //the difference in the two colors for the green channel
-//  int changeBlue  = endColor[2] - startColor[2];                                  //the difference in the two colors for the blue channel
-//  int steps = maxVal(abs(changeRed),maxVal(abs(changeGreen), abs(changeBlue)));   //make the number of change steps the maximum channel change
-//
-//  for(int i = 0 ; i < steps; i++){                                                //iterate for the channel with the maximum change
-//    byte newRed = startColor[0] + (i * changeRed / steps);                        //the newRed intensity dependant on the start intensity and the change determined above
-//    byte newGreen = startColor[1] + (i * changeGreen / steps);                    //the newGreen intensity
-//    byte newBlue = startColor[2] + (i * changeBlue / steps);                      //the newBlue intensity
-//    byte newColor[] = {newRed, newGreen, newBlue};                                //Define an RGB color array for the new color
-//    setColor(newC, newColor);                                                      //Set the LED to the calculated value
-//  }
-//  setColor(newC, endColor);                                                        //The LED should be at the endColor but set to endColor to avoid rounding errors
-//}
-//
