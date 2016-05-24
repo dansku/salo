42eab7b5538afe5c68690467f2ec1c6887811c3b
Daniel Andrade <daniel@danielandrade.net>
2016-05-24 22:49:39 +0200

    Choose if you want fadeToColor or not. Change most integers from int to uint8.

--- esp12/src/main.cpp
+++ esp12/src/main.cpp
@@ -16,20 +16,19 @@
 ThingerWebConfig thing;
 
 // Pins
-int leds[4] = {13,12,14,16}; // RED(D7) - GREEN(D6) - BLUE(D5) - WHITE(D0)
-int reset = 0;
+uint8_t leds[4] = {13,12,14,16};        // RED(D7) - GREEN(D6) - BLUE(D5) - WHITE(D0)
+uint8_t reset = 0;                      // Should we reset Wifi and Module settings
 
 // Variables
-int fadeSpeed = 15;                 //fading delay
-// #define maxVal(a,b) ((a)>(b)?(a):(b));  // Replacement for max()
-int oldC[4] = {0,0,0,0};                // Old colors
-int color[4] = {0,0,0,0};               // Red Green Blue White
+uint8_t fadeSpeed = 15;                     //fading delay
+uint8_t oldC[4]   = {0,0,0,0};              // Old colors
+uint8_t color[4]  = {0,0,0,0};              // Red Green Blue White
 
 
 // Set New Color
-void setColor(int* leds,int* color)
+void setColor(uint8_t* leds, uint8_t* color)
 {
-  for(int i = 0; i <= 0; i++)
+  for(int i = 0; i <= 3; i++)
   {
     Serial.print("Changing LED: ");
     Serial.print(leds[i]);
@@ -39,24 +38,24 @@ void setColor(int* leds,int* color)
   }
 }
 
-void fadeToColor(int* led, int* startColor, int* endColor, int fadeSpeed){
- int changeRed   = endColor[0] - startColor[0];                                  //the difference in the two colors for the red channel
- int changeGreen = endColor[1] - startColor[1];                                  //the difference in the two colors for the green channel
- int changeBlue  = endColor[2] - startColor[2];
- int changeWhite = endColor[3] - startColor[3];                                  //the difference in the two colors for the blue channel
- int steps = max(abs(changeRed),max(abs(changeGreen), abs(changeBlue)));         //make the number of change steps the maximum channel change
+void fadeToColor(uint8_t* leds, uint8_t* startColor, uint8_t* endColor, int fadeSpeed)
+{
+  int changeRed   = endColor[0] - startColor[0];                                  //the difference in the two colors for the red channel
+  int changeGreen = endColor[1] - startColor[1];                                  //the difference in the two colors for the green channel
+  int changeBlue  = endColor[2] - startColor[2];
+  int changeWhite = endColor[3] - startColor[3];                                  //the difference in the two colors for the blue channel
+  int steps = max(abs(changeRed),max(abs(changeGreen), abs(changeBlue)));         //make the number of change steps the maximum channel change
 
- for(int i = 0 ; i < steps; i++){                                                //iterate for the channel with the maximum change
-   int newRed      = startColor[0] + (i * changeRed / steps);                   //the newRed intensity dependant on the start intensity and the change determined above
-   int newGreen    = startColor[1] + (i * changeGreen / steps);                 //the newGreen intensity
-   int newBlue     = startColor[2] + (i * changeBlue / steps);
-   int newWhite    = startColor[3] + (i * changeWhite / steps);                  //the newBlue intensity
-   int newColor[]  = {newRed, newGreen, newBlue, newWhite};                     //Define an RGB color array for the new color
-   setColor(leds, newColor);
-   delay(fadeSpeed);                                                             //Set the LED to the calculated value
- }
+  for(int i = 0 ; i < steps; i++){                                                //iterate for the channel with the maximum change
+    uint8_t newRed      = startColor[0] + (i * changeRed / steps);                   //the newRed intensity dependant on the start intensity and the change determined above
+    uint8_t newGreen    = startColor[1] + (i * changeGreen / steps);                 //the newGreen intensity
+    uint8_t newBlue     = startColor[2] + (i * changeBlue / steps);
+    uint8_t newWhite    = startColor[3] + (i * changeWhite / steps);                  //the newBlue intensity
+    uint8_t newColor[]  = {newRed, newGreen, newBlue, newWhite};                     //Define an RGB color array for the new color
+    setColor(leds, newColor);
+    delay(fadeSpeed);                                                             //Set the LED to the calculated value
+  }
 }
-
 
 void setup()
 {
@@ -85,9 +84,16 @@ void setup()
     color[1] = in["green"];
     color[2] = in["blue"];
     color[3] = in["white"];
+    color[4] = in["fade"];
 
-    fadeToColor(leds, oldC, color, fadeSpeed);
-
+    if(color[4]==1)
+    {
+      fadeToColor(leds, oldC, color, fadeSpeed);
+    }
+    else
+    {
+      setColor(leds, color);
+    }
   };
 }
 
